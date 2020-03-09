a194ccb7fbb1df52fab977bd66be1ddd1e01a00f
rvtsukanov <Roman.Tsukanov@x5.ru>
2020-03-09 20:30:45 +0300

    logging added

--- metasaver.py
+++ metasaver.py
@@ -1,110 +1,149 @@
 import datetime
-import functools
-from typing import List, Callable
+from typing import List, Union, Tuple, Any
 import os
 import torch
 import numpy as np
 import re
+from sys import stdout
+import logging
 
 
 class MetaSaver:
-    def __init__(self, base_directory, postfix, **kwargs):
+    def __init__(self, base_directory: str, postfix: str, **kwargs):
+        '''
+        :param base_directory: root directory to proceed
+        :param postfix: postfix, used to distinguish different experiment types
+        '''
         self.base_directory = base_directory
         self.postfix = postfix
 
-    # def __call__(self, callable, attributes_to_save, *args, **kwargs):
-    #     self.setup_experiment(callable, attributes_to_save)
+        logging.basicConfig(filename=f'{__name__}.log',
+                            level=logging.INFO,
+                            # filemode='w+',
+                            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            datefmt='%d-%b-%y %H:%M:%S')
 
+        self.logger = logging.getLogger(f'{__name__}')
 
-    # def setup_experiment(self, callable: Callable,
-    #                      attributes_to_save: List[str]
-    #                      ):
-    #     @functools.wraps(callable)
-    #     def wrapper(*args, **kwargs):
-    #         self.name_dir = self._generate_name_dir()
-    #         result = callable(*args, **kwargs)
-    #
-    #         for attr in attributes_to_save:
-    #             if hasattr(self, attr):
-    #                 self._save_attribute(attr)
-    #             else:
-    #                 print('Bad attribute')
-    #         return result
-    #     return wrapper
-
-    def run_experiment(self, attributes_to_save, wrapper='run'):
+    def run_experiment(self,
+                       attributes_to_save: Union[List, Tuple],
+                       wrapper: str = 'run', **kwargs) -> Any:
+        '''
+        Wrapper. Usage: pass method to wrap into wrapper parameter. This function will create directory
+        named by current time before executing wrapper method of class. After, will save all attributes of class
+        (passed by 'attributes_to_save') to created directory.
+        :param attributes_to_save: attributes to save
+        :param wrapper: function to execute
+        :param kwargs: arguments used to pass to the wrapped method
+        :return: result of applying wrapper-function
+        '''
         if hasattr(self, wrapper):
+
             self.name_dir = self._generate_name_dir()
-            result = self.__getattribute__(wrapper)()
-            print(result)
+            logging.basicConfig(filename=os.path.join(self.base_directory, self.name_dir, f'{__name__}.log'),
+                                level=logging.INFO,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                datefmt='%d-%b-%y %H:%M:%S')
+
+            result = self.__getattribute__(wrapper)(**kwargs)
             for attr in attributes_to_save:
-                print(attr, hasattr(self, attr))
+                self.logger.info(f'Proceeding attribute: {attr}')
                 if hasattr(self, attr):
                     self._save_attribute(attr)
                 else:
-                    print('1Bad attribute')
+                    self.logger.error(f'{attr} is not an attribute of {self}. Not saved')
 
             return result
+
         else:
-            return
+            self.logger.error(f'{attr} is not an attribute of {self}')
+            raise Exception(f'{attr} is not an attribute of {self}')
 
 
-    def _generate_name_dir(self, postfix=None, time=None):
+    def _generate_name_dir(self, postfix: Union[None, str] = None,
+                           time: Union[datetime.datetime, str] = None) -> str:
+        '''
+        Name generation from current time
+        :return: str: directory name
+        '''
         if not time:
             now = datetime.datetime.now()
         else:
             now = time
         if postfix is None:
             postfix = self.postfix  # TODO: add hashing
-        return now.strftime('%d-%m-%Y--%H-%M-%S') + f'--{self.postfix}'
+        return now.strftime('%d-%m-%Y--%H-%M-%S') + f'--{postfix}'
 
 
-    # def save_model(self):
-    #     if not os.path.exists(f'{self.name_dir}'):
-    #         os.makedirs(f'./{self.name_dir}')
-    #     torch.save(self.state_dict(), f'{self.name_dir}/model')
-    #     return self
+    def _init_inner_logger(self, path):
+        handler = logging.FileHandler(os.path.join(self.base_directory, self.name_dir,
+                                                   f'{__name__}.log'))
+        formatter = logging.Formatter('"%(asctime)s - %(levelname)s - %(message)s"',
+                                      datefmt='%d-%b-%y %H:%M:%S')
 
+        handler.setFormatter(formatter)
+        self.logger_inner = logging.getLogger(f'{__name__}.log')
+        self.logger_inner.addHandler(handler)
 
-    def _save_attribute(self, attribute):
-
+    def _save_attribute(self, attribute: str) -> None:
+        '''
+        Method to save attribute
+        :param attribute: str, attribute to save
+        '''
         full_path = os.path.join(self.base_directory, self.name_dir)
-        print(full_path)
         if not os.path.exists(full_path):
             os.makedirs(os.path.join(self.base_directory, self.name_dir))
 
-        print('a', attribute, 't', type(attribute))
+        self._init_inner_logger(path=full_path)
+        self.logger_inner.info('Initializing inner log')
 
         if isinstance(self.__getattribute__(attribute), np.ndarray) or \
                 isinstance(self.__getattribute__(attribute), list):
-            print('saving loss')
-            np.save(os.path.join(full_path, attribute), np.array(attribute))
+            self.logger_inner.info(f'Saving attribute {attribute}')
+            try:
+                np.save(os.path.join(full_path, attribute), np.array(attribute))
+            except Exception as e:
+                self.logger_inner.error(f'Saving attribute {attribute} was not successfull. Error: {e}')
+
+            self.logger_inner.info(f'Attribute {attribute} saved successfuly')
 
         if hasattr(self, 'state_dict'):
-            print('saving model')
-            torch.save(self.state_dict(), os.path.join(full_path, 'model'))
+            self.logger.info('Saving model')
+            try:
+                torch.save(self.state_dict(), os.path.join(full_path, 'model'))
+            except Exception as e:
+                self.logger_inner.error(f'Saving model was not successfull. Error: {e}')
+            else:
+                self.logger_inner.info('Model saved successfully')
 
         else:
-            print('2Bad attribute')
+            self.logger.error(f'Model is not savable by torch. Proceeding pickling')
 
 
-    def load_model(self, dir):
+    def load_model(self, dir: str):
+        '''
+        Method to load model located in 'dir'. Possible to pass 'last' value in order to load last possible model (by)
+        directory name
+        :param dir: name of dir to load; possible to pass 'last'
+        :return: self-object (in order to use in pipeline)
+        '''
+        self.logger.info('Attempt to load model')
+        self.logger.info(f'Model tag: {dir}')
+
         if dir == 'last':
             date_names = []
             for name in os.listdir('./'):
                 try:
-                    # Дешево но что поделать
                     parsed_name = re.findall(r'\d{2}-\d{2}-\d{4}--\d{2}-\d{2}-\d{2}', name)
                     if parsed_name:
                         date_names.append(datetime.datetime.strptime(parsed_name[0], '%d-%m-%Y--%H-%M-%S'))
 
                 except ValueError as e:
-                    print(f'Name: {name} was not properly parsed. Error: {e}')
+                    self.logger.warning(f'Name: {name} was not properly parsed. Error: {e}')
 
 
             dir = sorted(date_names, reverse=True)[0]
-
-            print(f'Found maximum: {dir}, proceeding')
+            self.logger.info(f'{dir} is the last model found. Proceeding')
 
         self.load_state_dict(torch.load(os.path.join('.', self._generate_name_dir(self.postfix, time=dir), 'model')))
         return self
--- vae.py
+++ vae.py
@@ -3,21 +3,18 @@ import torch
 import torch.utils.data as data_utils
 import torch.optim as optim
 import numpy as np
-import datetime
 import os
 from torch.distributions.normal import Normal
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
-import re
 from metasaver import MetaSaver
 
 
 class VAE(nn.Module, MetaSaver):
-    def __init__(self, num_epochs, dim_z, train_set, postfix, base_directory):
+    def __init__(self, num_epochs, dim_z, train_set, postfix, base_directory, lr=1e-4):
         super(VAE, self).__init__()
-        MetaSaver.__init__(self, base_directory=base_directory, postfix=postfix)
-
-        # super(MetaSaver, self).__init__(base_directory, postfix)
+        MetaSaver.__init__(self, base_directory=base_directory,
+                           postfix=postfix)
 
         self.postfix = postfix
 
@@ -25,28 +22,29 @@ class VAE(nn.Module, MetaSaver):
         self.scaler.fit(train_set)
 
         self.train_set_scaled = self.scaler.transform(train_set)
-        self.train_df = data_utils.TensorDataset(torch.DoubleTensor(self.train_set_scaled), torch.zeros(train_set.shape[0]))
+        self.train_df = data_utils.TensorDataset(torch.DoubleTensor(self.train_set_scaled),
+                                                 torch.zeros(train_set.shape[0]))
+
         self.train_loader = data_utils.DataLoader(self.train_df, batch_size=32, shuffle=True)
 
         feat_shape = self.train_set_scaled.shape[1]
 
-        self.fc1 = nn.Linear(feat_shape, 32)
-        self.fc2 = nn.Linear(32, 32)
-        self.fc3 = nn.Linear(32, 16)
-        # self.fc4 = nn.Linear(64, 64)
-        self.fc51 = nn.Linear(16, dim_z)
-        self.fc52 = nn.Linear(16, dim_z)
+        self.fc_enc_1 = nn.Linear(feat_shape, 32)
+        self.fc_enc_2 = nn.Linear(32, 32)
+        self.fc_enc_3 = nn.Linear(32, 16)
+        self.fc_enc_41 = nn.Linear(16, dim_z)
+        self.fc_enc_42 = nn.Linear(16, dim_z)
 
-        self.fc_o1 = nn.Linear(dim_z, 16)
-        # self.fc_o2 = nn.Linear(64, 64)
-        self.fc_o3 = nn.Linear(16, 32)
-        self.fc_o4 = nn.Linear(32, 32)
-        self.fc_o51 = nn.Linear(32, feat_shape)
-        self.fc_o52 = nn.Linear(32, feat_shape)
+        self.fc_dec_1 = nn.Linear(dim_z, 16)
+        self.fc_dec_2 = nn.Linear(16, 32)
+        self.fc_dec_3 = nn.Linear(32, 32)
+        self.fc_dec_41 = nn.Linear(32, feat_shape)
+        self.fc_dec_42 = nn.Linear(32, feat_shape)
         self.relu = nn.ReLU()
 
         self.num_epochs = num_epochs
-        self.optimizer = optim.Adam(self.parameters(), lr=1e-4)
+        self.optimizer = optim.Adam(self.parameters(),
+                                    lr=lr)
 
 
     def gaussian_sampler(self, mu, logvar):
@@ -55,21 +53,22 @@ class VAE(nn.Module, MetaSaver):
         return mu + eps * std
 
     def encode(self, x):
-        h1 = self.relu(self.fc1(x))
-        h2 = self.relu(self.fc2(h1))
-        h3 = self.relu(self.fc3(h2))
-        # h4 = self.relu(self.fc4(h3))
-        return self.fc51(h3), torch.sigmoid(self.fc52(h3))
+        h1 = self.relu(self.fc_enc_1(x))
+        h2 = self.relu(self.fc_enc_2(h1))
+        h3 = self.relu(self.fc_enc_3(h2))
+        return self.fc_enc_41(h3), torch.sigmoid(self.fc_enc_42(h3))
 
     def decode(self, z):
-        h1 = self.relu(self.fc_o1(z))
-        # h2 = self.relu(self.fc_o2(h1))
-        h3 = self.relu(self.fc_o3(h1))
-        h4 = self.relu(self.fc_o4(h3))
-        return self.fc_o51(h4), self.fc_o52(h4)
+        h1 = self.relu(self.fc_dec_1(z))
+        h2 = self.relu(self.fc_dec_2(h1))
+        h3 = self.relu(self.fc_dec_3(h2))
+        return self.fc_dec_41(h3), self.fc_dec_42(h3)
 
     def forward(self, x):
-        # TODO
+        '''
+        :param x:
+        :return:
+        '''
         latent_mu, latent_logsigma = self.encode(x)
         z = self.gaussian_sampler(latent_mu, latent_logsigma)
         reconstruction_mu, reconstruction_logsigma = self.decode(z)
@@ -91,7 +90,6 @@ class VAE(nn.Module, MetaSaver):
         self.postfix = new_postfix
 
     def run(self):
-        # self.name_dir = self.generate_name_dir()
         self.train_loss_epoch = []
         for epoch in range(self.num_epochs):
             # self.train() #TODO: rename
@@ -103,15 +101,16 @@ class VAE(nn.Module, MetaSaver):
                 mu_reconsct, logsigma_reconsct, mu_noise, logsigma_noise = self(obj.float())
 
                 loss = self.loss_vae(obj, mu_reconsct, logsigma_reconsct, mu_noise, logsigma_noise)
-                print(loss)
+                # print(loss)
 
                 loss.backward()
                 self.optimizer.step()
                 train_loss.append(loss.data.numpy())
             self.train_loss_epoch.append(np.mean(train_loss))
             if epoch % 10 == 0:
-                plt.plot(np.array(self.train_loss_epoch))
-                plt.show()
+                pass
+                # plt.plot(np.array(self.train_loss_epoch))
+                # plt.show()
         return self
 
 
@@ -136,6 +135,10 @@ class VAE(nn.Module, MetaSaver):
 
     def plot_loss(self, dir=None):
         pass
+
+
+    def render_configuration_into_file(self):
+        pass #TODO
 
 
     def generate_from_noise(self, batch_of_input_data, num_of_sampled):
